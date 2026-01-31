eb5918239fc02c1e439575fa79a01585248bd9af
Leonardo Mengozzi <leonardo.mengozzi2@studio.unibo.it>
2026-01-31 11:00:46 +0100

    fix(enemies): adjusted probability ditribution of enemy spawn

--- src/main/java/it/unibo/wildenc/mvc/model/enemies/EnemySpawnerImpl.java
+++ src/main/java/it/unibo/wildenc/mvc/model/enemies/EnemySpawnerImpl.java
@@ -20,9 +20,12 @@ public class EnemySpawnerImpl implements EnemySpawner{
     private static final int DISTANCE_SPAWN_X = 30;
     private static final int DISTANCE_SPAWN_Y = 13;
     private static final int BASE_ENEMY = 10;
-    private static final double LOAD_SSFACTOR = 1.3;
+    private static final double LOAD_FACTOR = 1.3;
+    private static final int PROBABILITY_DISTRIBUTION = 16;
     private final Random rand;
     private final EnemyFactory ef;
+
+    //TODO: enum dei nomi dei pokemon.
 
     public EnemySpawnerImpl(final MapObject target) {
         this.rand = new Random();
@@ -40,13 +43,13 @@ public class EnemySpawnerImpl implements EnemySpawner{
                     rand.nextInt(-DISTANCE_SPAWN_X, DISTANCE_SPAWN_X), 
                     rand.nextInt(-DISTANCE_SPAWN_Y, DISTANCE_SPAWN_Y)
                 );
-                return switch (i % BASE_ENEMY) {
-                    case 1, 2, 3 -> this.ef.CloseRangeEnemy(v, "Pippo");
-                    case 4, 5 -> this.ef.CloseRangeFastEnemy(v, "PaperoniDePaperoni");
-                    case 6, 7 -> this.ef.RangedEnemy(v, "Topolino");
-                    case 8 -> this.ef.RangedDoubleShotEnemy(v, "Topolina");
-                    case 9 -> this.ef.RoamingEnemy(v, "Pluto");
-                    case 10 -> this.ef.RoamingLongLifeEnemy(v, "Chip");
+                return switch (i % PROBABILITY_DISTRIBUTION) {
+                    case 1, 2, 3, 14, 15 -> this.ef.CloseRangeEnemy(v, "Pippo");
+                    case 4, 5, 6 -> this.ef.CloseRangeFastEnemy(v, "PaperoniDePaperoni");
+                    case 7, 8, 13 -> this.ef.RangedEnemy(v, "Topolino");
+                    case 9, 10 -> this.ef.RangedDoubleShotEnemy(v, "Topolina");
+                    case 11, 16 -> this.ef.RoamingEnemy(v, "Pluto");
+                    case 12 -> this.ef.RoamingLongLifeEnemy(v, "Chip");
                     default -> this.ef.CloseRangeEnemy(v, "Paperino");
                 };
             }).collect(Collectors.toSet());
