f0a2dd222de2709a3bc7745edf26339a659299db
Kleo Rama <kleo.rama@studio.unibo.it>
2026-02-04 19:33:40 +0100

    feat(Collectible): added apply method to Collectible interface

--- src/main/java/it/unibo/wildenc/mvc/model/Collectible.java
+++ src/main/java/it/unibo/wildenc/mvc/model/Collectible.java
@@ -1,9 +1,15 @@
 package it.unibo.wildenc.mvc.model;
 
 /**
- * A collectible item on the Map, such as Experience, Health drops and others...
+ * A collectible item on the Map, such as Experience, Health drops and others
  */
 public interface Collectible extends MapObject {
+
+    /**
+     * Applies the effect of the collectible to the player
+     * @param target the player collecting the item.
+     */
+    void apply(Player target);
 
     /**
      * Gets the value of the collectible
